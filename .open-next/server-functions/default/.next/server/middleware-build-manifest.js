5939c7f8640f451d0389acebf5c04dc87cdb8806
ZZC <zzcreation2022@gmail.com>
2026-04-02 15:56:11 +0800

    fix: add _worker.js and _routes.json for Cloudflare Pages

---
+++ .open-next/server-functions/default/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/03~yq9q893hmn.js"
+  ],
+  "lowPriorityFiles": [
+    "static/idZW0VngYRa4a8Dl3qLrN/_buildManifest.js",
+    "static/idZW0VngYRa4a8Dl3qLrN/_ssgManifest.js",
+    "static/idZW0VngYRa4a8Dl3qLrN/_clientMiddlewareManifest.js"
+  ],
+  "rootMainFiles": [
+    "static/chunks/0ht900cau6_ur.js",
+    "static/chunks/0p5.qr0b94vxo.js",
+    "static/chunks/0bogtdbh.dcu1.js",
+    "static/chunks/turbopack-103kkkinvnsda.js"
+  ]
+};
---
+++ .open-next/server-functions/default/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ROaZe7FjDQXEPr2Ym6VpgpFIHyXlOhPDDJtllJMmgQw=\"\n}"
